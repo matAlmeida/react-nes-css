63daba7bc0bd8bf357d4005d8520ace360d1c97e
Matheus Almeida <mat.almeida@live.com>
2018-12-10 19:07:44 -0300

    test: Container Test

---
+++ src/__tests__/Container.test.tsx
@@ -0,0 +1,93 @@
+import React from "react";
+import renderer from "react-test-renderer";
+
+import Container from "../lib/Container";
+
+describe("Container", () => {
+  it("renders without crashing", () => {
+    const component = <Container />;
+    const rendered = renderer.create(component).toJSON();
+
+    expect(rendered).toBeTruthy();
+  });
+
+  it("renders with title", () => {
+    const component = <Container title="Test" />;
+    const rendered = renderer.create(component).toJSON();
+
+    expect(rendered).toBeTruthy();
+  });
+
+  it("renders with children", () => {
+    const component = <Container title="Test">Test</Container>;
+    const rendered = renderer.create(component).toJSON();
+
+    expect(rendered).toBeTruthy();
+  });
+
+  it("renders with style", () => {
+    const component = (
+      <Container title="Test" style={{ background: "#e2e2e2" }} />
+    );
+    const rendered = renderer.create(component).toJSON();
+
+    expect(rendered).toBeTruthy();
+  });
+
+  describe("renders with flags", () => {
+    describe("one", () => {
+      it("center", () => {
+        const component = <Container title="Test" center />;
+        const rendered = renderer.create(component).toJSON();
+
+        expect(rendered).toBeTruthy();
+      });
+
+      it("rounded", () => {
+        const component = <Container title="Test" rounded />;
+        const rendered = renderer.create(component).toJSON();
+
+        expect(rendered).toBeTruthy();
+      });
+
+      it("dark", () => {
+        const component = <Container title="Test" dark />;
+        const rendered = renderer.create(component).toJSON();
+
+        expect(rendered).toBeTruthy();
+      });
+    });
+
+    describe("two", () => {
+      it("center rounded", () => {
+        const component = <Container title="Test" center rounded />;
+        const rendered = renderer.create(component).toJSON();
+
+        expect(rendered).toBeTruthy();
+      });
+
+      it("center dark", () => {
+        const component = <Container title="Test" center dark />;
+        const rendered = renderer.create(component).toJSON();
+
+        expect(rendered).toBeTruthy();
+      });
+
+      it("rounded dark", () => {
+        const component = <Container title="Test" rounded dark />;
+        const rendered = renderer.create(component).toJSON();
+
+        expect(rendered).toBeTruthy();
+      });
+    });
+
+    describe("three", () => {
+      it("center rounded dark", () => {
+        const component = <Container title="Test" center rounded dark />;
+        const rendered = renderer.create(component).toJSON();
+
+        expect(rendered).toBeTruthy();
+      });
+    });
+  });
+});
