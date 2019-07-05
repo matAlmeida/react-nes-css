9cbd9039db8cf1aa07ca31b84d5d2bf655de47d1
Matheus Almeida <mat.almeida@live.com>
2019-07-05 02:49:00 -0300

    docs(page): fix spacing

--- src/index.tsx
+++ src/index.tsx
@@ -1,39 +1,61 @@
 import React from "react";
 import { render } from "react-dom";
-import { Container, Button, Field, Balloon } from "./lib";
+import { Text, Container, Button, Field, Balloon } from "./lib";
 
 const App = () => (
   <div>
-    <Container title="Buttons">
-      <Button label="Normal" />
-      <Button label="Primary" primary />
-      <Button label="Success" success />
-      <Button label="Warning" warning />
-      <Button label="Error" error />
-      <Button label="Disabled" disabled />
+    <Container title="Texts" style={{ margin: "1em" }}>
+      <Text style={{ margin: "1em" }}>Normal</Text>
+      <Text primary style={{ margin: "1em" }}>
+        Primary
+      </Text>
+      <Text success style={{ margin: "1em" }}>
+        Success
+      </Text>
+      <Text warning style={{ margin: "1em" }}>
+        Warning
+      </Text>
+      <Text error style={{ margin: "1em" }}>
+        Error
+      </Text>
+      <Text disabled style={{ margin: "1em" }}>
+        Disabled
+      </Text>
     </Container>
-    <Container title="Containers">
-      <Container title="Container.is-center" center>
+
+    <Container title="Buttons" style={{ margin: "1em" }}>
+      <Button label="Normal" style={{ margin: "1em" }} />
+      <Button label="Primary" primary style={{ margin: "1em" }} />
+      <Button label="Success" success style={{ margin: "1em" }} />
+      <Button label="Warning" warning style={{ margin: "1em" }} />
+      <Button label="Error" error style={{ margin: "1em" }} />
+      <Button label="Disabled" disabled style={{ margin: "1em" }} />
+    </Container>
+
+    <Container title="Containers" style={{ margin: "1em" }}>
+      <Container title="Container.is-center" center style={{ margin: "1em" }}>
         Good morning. Thou hast had a good night's sleep, I hope.
       </Container>
-      <Container title="Container.is-dark" dark>
+      <Container title="Container.is-dark" dark style={{ margin: "1em" }}>
         Good morning. Thou hast had a good night's sleep, I hope.
       </Container>
-      <Container rounded>
+      <Container rounded style={{ margin: "1em" }}>
         Good morning. Thou hast had a good night's sleep, I hope.
       </Container>
-      <Container rounded dark>
+      <Container rounded dark style={{ margin: "1em" }}>
         Good morning. Thou hast had a good night's sleep, I hope.
       </Container>
     </Container>
-    <Container title="Form">
+
+    <Container title="Form" style={{ margin: "1em" }}>
       <Field label="Your Name" />
       <Field label=".input.is-success" placeholder="NES.css" success inline />
       <Field label=".input.is-warning" placeholder="8bit.css" warning inline />
       <Field label=".input.is-error" placeholder="awesome.css" error inline />
       <Field label=".input.is-dark" placeholder="dark.css" dark inline />
     </Container>
-    <Container title="Balloons">
+
+    <Container title="Balloons" style={{ margin: "1em" }}>
       <Balloon left>Hello NES.css</Balloon>
       <Balloon>
         Good morning. Thou hast had a good night's sleep, I hope.
